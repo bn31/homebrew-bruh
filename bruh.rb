6bfbd1d6a3d90f4da9a004706cae92430aa86d03
Brad <111555155+bn31@users.noreply.github.com>
2025-08-01 17:13:11 -0700

    Update bruh.rb
    new sha256 for release 0.0.2-alpha

--- bruh.rb
+++ bruh.rb
@@ -2,7 +2,7 @@ class Bruh < Formula
   desc "Play bruh sound from the command line"
   homepage "https://github.com/bn31/homebrew-bruh"
   url "https://github.com/bn31/homebrew-bruh/releases/download/v0.0.1-alpha/bruh.tar.gz"
-  sha256 "7fdae3a75294dd036b6a5eb2573c8940147c84324c76bdc357afd03bcb760353"
+  sha256 "eb3cde11126322f23c387671b7622453d45a27c1ac67e8ae3112b50b1fdcc577"
   version "1.0.0"
 
   def install
