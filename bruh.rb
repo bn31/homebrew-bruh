9bbe792dd3abe292b6eff8926b3f71feb7332d54
Brad <111555155+bn31@users.noreply.github.com>
2025-08-01 17:30:33 -0700

    Update bruh.rb

--- bruh.rb
+++ bruh.rb
@@ -1,7 +1,7 @@
 class Bruh < Formula
   desc "Play bruh sound from the command line"
   homepage "https://github.com/bn31/homebrew-bruh"
-  url "https://github.com/bn31/homebrew-bruh/releases/download/v0.0.1-alpha/bruh.tar.gz"
+  url "https://github.com/bn31/homebrew-bruh/releases/download/v0.0.2-alpha/bruh.tar.gz"
   sha256 "eb3cde11126322f23c387671b7622453d45a27c1ac67e8ae3112b50b1fdcc577"
   version "1.0.0"
 
